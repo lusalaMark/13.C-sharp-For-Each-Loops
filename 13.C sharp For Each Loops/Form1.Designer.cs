571186a76339b1edb6549382b0ebedef935d1d58
Mark Lusala Mudoga <lusalamark63@gmail.com>
2021-03-23 16:40:46 +0300

    First Commit

---
+++ 13.C sharp For Each Loops/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace _13.C_sharp_For_Each_Loops
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.lblNumbers = new System.Windows.Forms.Label();
+			this.lblLetters = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// lblNumbers
+			// 
+			this.lblNumbers.AutoSize = true;
+			this.lblNumbers.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F);
+			this.lblNumbers.Location = new System.Drawing.Point(507, 204);
+			this.lblNumbers.Name = "lblNumbers";
+			this.lblNumbers.Size = new System.Drawing.Size(0, 63);
+			this.lblNumbers.TabIndex = 9;
+			// 
+			// lblLetters
+			// 
+			this.lblLetters.AutoSize = true;
+			this.lblLetters.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F);
+			this.lblLetters.Location = new System.Drawing.Point(12, 204);
+			this.lblLetters.Name = "lblLetters";
+			this.lblLetters.Size = new System.Drawing.Size(0, 63);
+			this.lblLetters.TabIndex = 8;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F);
+			this.label2.Location = new System.Drawing.Point(507, 106);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(246, 63);
+			this.label2.TabIndex = 7;
+			this.label2.Text = "Numbers";
+			this.label2.Click += new System.EventHandler(this.label2_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F);
+			this.label1.Location = new System.Drawing.Point(12, 106);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(192, 63);
+			this.label1.TabIndex = 6;
+			this.label1.Text = "Letters";
+			this.label1.Click += new System.EventHandler(this.label1_Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F);
+			this.textBox1.Location = new System.Drawing.Point(12, 12);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(829, 68);
+			this.textBox1.TabIndex = 5;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(855, 450);
+			this.Controls.Add(this.lblNumbers);
+			this.Controls.Add(this.lblLetters);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textBox1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label lblNumbers;
+		private System.Windows.Forms.Label lblLetters;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBox1;
+	}
+}
+
---
+++ 13.C sharp For Each Loops/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _13.C_sharp_For_Each_Loops
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void label1_Click(object sender, EventArgs e)
+		{
+			lblLetters.Text = "";//clear value of text property so output does not accumulate
+								 //123%46(*)A
+								 //grab each character, hold it and decide whether it's a letter or digit
+								 //grab 1, decide it's a number
+								 //grab A, examine it, decide it's a letter and display
+								 //for each character in the box text property
+			foreach (var c in textBox1.Text)
+			{
+				if (char.IsLetter(c))//check each character from the string to determine whether it's a letter
+					lblLetters.Text += $"{c}\n";//if a character is a letter, print it
+			}
+		}
+
+		private void label2_Click(object sender, EventArgs e)
+		{
+			lblNumbers.Text = "";//clear value of text so output does not accumulate between runs
+								 //grab each letter in the string inside the text box
+								 //12$5GA imagine this is the string, so as the loop runs, c is 1,2,$,5,G,A
+			foreach (var c in textBox1.Text)
+			{
+				if (char.IsDigit(c))//examine each character to check whether it's a digit
+					lblNumbers.Text += $"{c}\n";//print the character if it's a digit
+			}
+		}
+	}
+}
